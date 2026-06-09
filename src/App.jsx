2499d69f71ec6167287538ef874e06ca26ebe257
Francesco Floris <francescofloris.ef@gmail.com>
2026-06-09 16:25:34 +0200

    Remove unnecessary whitespace and clean up code formatting in App component

--- src/App.jsx
+++ src/App.jsx
@@ -8,39 +8,36 @@ function App() {
         setFilter(event.target.value);
     };
     const visibleEvents = events;
-  const visibleCount = visibleEvents.length;
-    return ( <>
+    const visibleCount = visibleEvents.length;
+    return (<>
         <header>
-            
+
         </header>
         <main>
-             <div>
-            <p value={filter} onChange={changeHandler}>FIltra per Categoria:</p>
-            <select class="form-select" aria-label="Default select example">
-                <option value="tutti">Tutti</option>
-                <option value="conferenze">Conferenze</option>
-                <option value="workshop">Workshop</option>
-                <option value="meetup">Meetup</option>
-            </select>
-        </div>
-        <p>Eventi trovati: {visibleCount}</p>
+            <div>
+                <p value={filter} onChange={changeHandler}>FIltra per Categoria:</p>
+                <select class="form-select" aria-label="Default select example">
+                    <option value="tutti">Tutti</option>
+                    <option value="conferenze">Conferenze</option>
+                    <option value="workshop">Workshop</option>
+                    <option value="meetup">Meetup</option>
+                </select>
+            </div>
+            <p>Eventi trovati: {visibleCount}</p>
 
-          {/*lista eventi*/}
+            {/*lista eventi*/}
 
-      <ul>
-        {visibleEvents.map((event) => (
-          <li key={event.id}>{event.title}</li>
-        ))}
-      </ul>
+            <ul>
+                {visibleEvents.map((event) => (
+                    <li key={event.id}>{event.title}</li>
+                ))}
+            </ul>
         </main>
         <footer>
-            
+
         </footer>
-    </>     
-    )};
-
-   
-
-}
+    </>
+    )
+};
 
 export default App;
