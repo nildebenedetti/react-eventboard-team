6b5e9262300165d419b2c2d1cc73d67640d4d35c
Francesco Floris <francescofloris.ef@gmail.com>
2026-06-09 16:10:06 +0200

    add: implement category filter in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-import './App.css'
+import { useState } from "react"
 
 function App() {
- 
-}
+    const [filter, setFilter] = useState('Tutti');
+    const changeHandler = (event) => {
+        setFilter(event.target.value);
+    };
+    return (
+        <div>
+            <p value={filter} onChange={changeHandler}>FIltra per Categoria:</p>
+            <select class="form-select" aria-label="Default select example">
+                <option value="tutti">Tutti</option>
+                <option value="conferenze">Conferenze</option>
+                <option value="workshop">Workshop</option>
+                <option value="meetup">Meetup</option>
+            </select>
+        </div>
+    )
+};
 
-export default App
+export default App;
