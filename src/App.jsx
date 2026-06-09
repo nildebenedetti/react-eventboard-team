d8d25457820e05f7dae2232c33f0717ac1ffc494
Raffaele De Pasca <depascaraffaele@gmail.com>
2026-06-09 16:16:15 +0200

    issue 4 risolta

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import { events } from "./data/events";
+
 function App() {
- return;
+  const visibleEvents = events;
+  const visibleCount = visibleEvents.length;
+
+  return (
+    <main>
+      <p>Eventi trovati: {visibleCount}</p>
+
+      {/*lista eventi*/}
+
+      <ul>
+        {visibleEvents.map((event) => (
+          <li key={event.id}>{event.title}</li>
+        ))}
+      </ul>
+    </main>
+  );
 }
 
 export default App
