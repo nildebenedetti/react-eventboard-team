9b7eeb2c0eb37d21a21791d0ffcdbbee1a84669e
“Nilde <nilde.benedetti@gmail.com>
2026-06-09 16:42:11 +0200

    aggiunto altri campi card +  badge

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,15 +1,18 @@
+
 function EventCard(event) {
+
     const { title, date, category, status } = event;
+    const isOpen = status === 'aperto' || status === 'chiuso';
 
-
-  return <>
-  <div className="card">
-        <h2 className="card-title fw-bold">${title}</h2>
-        <div className="card-body">
-            <p className="date fw-3 py-3"><span>Date:</span>${date}</p>
-            <p className="category fw-3 py-3"><span>Category</span>${category}</p>
+    return <>
+        <div className="card">
+            <h2 className="card-title fw-bold">${title}<span class={`badge ${isOpen ? 'bg-success' : 'bg-danger'}`}>{isOpen ? 'Aperto' : 'Chiuso'}</span></h2>
+            <div className="card-body">
+                <p className="date fw-3 py-3"><span>Date:</span>${date}</p>
+                <p className="category fw-3 py-3"><span>Category:</span>${category}</p>
+                <p className="category fw-3 py-3"><span>Status:</span>${status}</p>
+            </div>
         </div>
-  </div>
-  </>
+    </>
 }
 export default EventCard;
